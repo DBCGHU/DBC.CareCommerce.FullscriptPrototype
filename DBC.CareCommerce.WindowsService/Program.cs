1bd157bc1346f180a78d3f28913d9c507684fede
DBCGHU <support@quick-charts.com>
2026-06-09 08:44:34 -0500

    Wire Windows Service dependencies through DI

--- DBC.CareCommerce.WindowsService/Program.cs
+++ DBC.CareCommerce.WindowsService/Program.cs
@@ -1,3 +1,10 @@
+using System;
+using DBC.CareCommerce.Application.Services;
+using DBC.CareCommerce.Contracts.Repositories;
+using DBC.CareCommerce.Contracts.Services;
+using DBC.CareCommerce.Contracts.Services.Contracts;
+using DBC.CareCommerce.Data.DataAccess;
+using DBC.CareCommerce.Data.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -18,6 +25,39 @@ namespace DBC.CareCommerce.WindowsService
                 {
                     services.Configure<CareCommerceServiceSettings>(
                         hostContext.Configuration.GetSection("CareCommerceService"));
+
+                    services.AddSingleton(provider =>
+                    {
+                        CareCommerceServiceSettings settings =
+                            hostContext.Configuration
+                                .GetSection("CareCommerceService")
+                                .Get<CareCommerceServiceSettings>() ?? new CareCommerceServiceSettings();
+
+                        string connectionString =
+                            Environment.GetEnvironmentVariable("DBC_CARECOMMERCE_SQL_CONNECTION");
+
+                        if (string.IsNullOrWhiteSpace(connectionString))
+                        {
+                            connectionString = settings.SqlConnectionString;
+                        }
+
+                        if (string.IsNullOrWhiteSpace(connectionString))
+                        {
+                            connectionString =
+                                hostContext.Configuration["CareCommerceService:SqlConnectionString"];
+                        }
+
+                        return new SqlConnectionFactory(connectionString);
+                    });
+
+                    services.AddScoped<ICatalogItemRepository, SqlCatalogItemRepository>();
+                    services.AddScoped<ICareItemRepository, SqlCareItemRepository>();
+                    services.AddScoped<IPendingChargeRepository, SqlPendingChargeRepository>();
+                    services.AddScoped<IFullscriptTransactionRepository, SqlFullscriptTransactionRepository>();
+
+                    services.AddScoped<ICareItemApplicationService, CareItemApplicationService>();
+                    services.AddScoped<ICareCommerceIntegrationService, CareCommerceIntegrationService>();
+                    services.AddScoped<FullscriptTransactionDispatcherService>();
 
                     services.AddHostedService<Worker>();
                 });
